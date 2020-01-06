7dc85d04732f97cee316cc77c173e07a614a1334
Anders Mølmen Høst <andersm_host@hotmail.com>
2020-01-06 13:18:16 +0100

    Update

--- src/hw_func.py
+++ src/hw_func.py
@@ -15,15 +15,27 @@ Ex 3.20: Write functions
 """
 
 def hw1():
+    """
+    returns a string
+    """
     return("Hello, world!")
 
 def hw2():
+    """
+    prints string directly
+    """
     print("Hello, world!")
 
 def hw3(word1, word2):
+    """
+
+    :param word1: the first argument is a word
+    :param word2: the second argument is another word
+    :return: the two words together
+    """
     return word1 + word2
 
-print(hw1())    
+print(hw1())
 hw2()
 print(hw3("Hello, ", "World!"))
 print(hw3("Python ", "function"))
