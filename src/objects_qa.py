021e904213fb3f4a911e9b393d4349dd8dd59066
Anders Mølmen Høst <andersm_host@hotmail.com>
2020-01-20 17:47:20 +0100

    added ex4.6

---
+++ src/objects_qa.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'Anders Mølmen Høst'
+__email__ = 'anderhos@nmbu.no'
+
+"""
+From
+
+Book: A Primer on Scientific Programming with Python
+Author: Hans Petter Langtangen
+Editition: 5th Edition
+Year: 2016
+
+Exercise 4.6: Read input from the keyboard 
+
+"""
+
+eval = eval(input("input = "))
+print(type(eval))
+print(eval)
