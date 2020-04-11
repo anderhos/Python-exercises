92eeec01f411abd2ce8286babeb860147edd1cd3
Anders Mølmen Høst <andersm_host@hotmail.com>
2020-04-11 15:12:26 +0200

    update ex5.1

--- src/fill_lists.py
+++ src/fill_lists.py
@@ -8,7 +8,7 @@ From
 
 Book: A Primer on Scientific Programming with Python
 Author: Hans Petter Langtangen
-Editition: 5th Edition
+Edition: 5th Edition
 Year: 2016
 Exercise 5.1: Fill lists with function values
 
@@ -16,12 +16,17 @@ Exercise 5.1: Fill lists with function values
 
 import numpy as np
 
+
 def h(x):
     return 1/np.sqrt(2*np.pi) * np.exp(-0.5*x**2)
-n = 41
-dx = 8.0/(n-1)
-xlist = [i*dx for i in range(n)]
-hlist = [h(x) for x in xlist]
 
-print(xlist)
-print(hlist)
+
+n = 41
+interval = len(range(-4, 4))    # The length of the interval of x
+dx = interval/(n-1)    # Length of step
+x0 = -4    # Starting value for x
+xlist = [x0 + i*dx for i in range(n)]
+ylist = [h(x) for x in xlist]
+list_pairs = [[x, y] for x, y in zip(xlist, ylist)]
+
+print(list_pairs)
