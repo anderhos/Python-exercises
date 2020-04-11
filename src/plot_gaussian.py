ba884fc0e1d519c9e18a1a983516464b10db54c4
Anders Mølmen Høst <andersm_host@hotmail.com>
2020-04-11 16:35:18 +0200

    update ex5.4

---
+++ src/plot_gaussian.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'Anders Mølmen Høst'
+__email__ = 'anderhos@nmbu.no'
+
+"""
+From
+
+Book: A Primer on Scientific Programming with Python
+Author: Hans Petter Langtangen
+Edition: 5th Edition
+Year: 2016
+Exercise 5.4: Plot a function
+
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def h(x):
+    return 1/np.sqrt(2*np.pi) * np.exp(-0.5*x**2)
+
+
+n = 41
+start = -4
+stop = 4
+dx = 1/(n-1)
+
+x = np.linspace(start, stop, n)
+
+# fill array y with function values
+y = h(x)
+
+# plot the function
+plt.plot(x, h(x))
+plt.show()
