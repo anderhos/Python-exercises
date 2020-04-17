77008d5534a0c0bd57c14a72c9e0c8a19c759cc7
Anders Mølmen Høst <andersm_host@hotmail.com>
2020-04-17 18:22:18 +0200

    update ex6.5

--- src/stars_data_dict1.py
+++ src/stars_data_dict1.py
@@ -1,4 +1,4 @@
-        # -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 
 __author__ = 'Anders Mølmen Høst'
 __email__ = 'anderhos@nmbu.no'
@@ -21,22 +21,47 @@ stars in first column in file and luminosity in fourth column
 """
 
 """
-File not working as it should. Only first name of star and returns
-a list within a dictionary it seems
+File not working as it should. Get a dictionary, but keys and values are
+not as they should be.
 """
 
 """
-Try new implementation. Remove 2 columns, second last and third last
+Remember: In the file we have a list of four tuples, not a table as in
+e.g density.py
 """
 
-"""
-A list of four tuples, not a table as in density.py
-"""
+
 def read_stars(filename):
+
     infile = open(filename, "r")
-    lines = [line for line in infile]
+    # skipping first line
+    infile.readline()
+    luminosities = {}
+    for line in infile:
+        words = line.split()
+        # luminosity is the last word in line
+        luminosity = words[-1]
+        # type luminosity is a string
+        # Need to remove characters ), so we can convert to float
+        # removing the three last words to get star name
+        if len(words[:-3]) == 4:
+            star = words[0] + ' ' + words[1] + ' ' + words[2] + ' ' + words[3]
+        elif len(words[:-3]) == 3:
+            star = words[0] + ' ' + words[1] + ' ' + words[2]
+        elif len(words[:-3]) == 2:
+            star = words[0] + ' ' + words[1]
+        else:
+            star = words[0]
+        # type star is a string but has extra character ( in the beginning
+        # of every name
+
+        luminosities[star] = luminosity
     infile.close()
-    return lines
+    return luminosities
 
-print(read_stars("stars.txt"))
 
+output = read_stars("stars.txt")
+print(output)
+"""
+keys and values are correct but with extra characters around as mentioned
+"""
