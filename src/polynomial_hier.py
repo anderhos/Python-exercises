0bc50fb5ca441e23768d1b7d0a9c8725ed79832d
Anders Høst <andersm_host@hotmail.com>
2020-05-26 17:55:58 +0200

    update ex9.4

--- src/polynomial_hier.py
+++ src/polynomial_hier.py
@@ -13,8 +13,11 @@ Year: 2016
 Exercise 9.4: Create an alternative class hierarchy for polynomials
 
 """
+"""
+Source: https://www.uio.no/studier/emner/matnat/ifi/IN1900/h19/ressurser/
+live_programmering/polynomial_hier.py
+ """
 
-# Source HPL p 444
 class Polynomial:
     def __init__(self, coefficients):
         self.coeff = coefficients
@@ -47,31 +50,83 @@ class Polynomial:
         else:
             result_coeff = other.coeff[:]    # copy
             for i in range(len(self.coeff)):
-                result_coeff[i] += self.coeff
+                result_coeff[i] += self.coeff[i]
         return Polynomial(result_coeff)
 
-class Parabola(Polynomial):
-    """
-    Inherit coefficients from Polynomial. Set extra coefficients to zero
-    """
-    def __init__(self, c0, c1, c2):
-        super().__init__(coefficients=[c0, c1, c2])
 
-    def __call__(self, x):
-        """Evaluate parabola"""
-        return self.coeff[0] + self.coeff[1] * x + self.coeff[2] * x**2
+    def __sub__(self, other):
+        """Return self - other as Polynomial object.
+               As for the add function above, we have two cases
+               that must be treated separately: one where
+               self.coeff is longer than other.coeff,
+               one where other.coeff is longest. The difference
+               from the __add__ method is that for subtraction
+               the order matters, so the implementation
+               must be slightly different
+               """
+        # start with the simplest case where self.coeff is longest
+        if len(self.coeff) > len(other.coeff):
+            result_coeff = self.coeff[:]    # copy
+            for i in range(len(other_coeff)):
+                result_coeff[i] -= other.coeff[i]
+        else:
+            # Here, the polynomial we subtract is the
+            # longest, which requires some extra care:
+            result_coeff = [0]*len(other.coeff)
+            for i in range(len(self.coeff)):
+                result_coeff[i] = self.coeff[i]
 
-class Line(Parabola):
-    def __init__(self, c0, c1, c2=0):
-        super().__init__(c0, c1, c2)
+            for i in range(len(other.coeff)):
+                result_coeff[i] -= other.coeff[i]
 
-    def table(self, L, R, n):
-        """Return a table with n points for L <= x <= R."""
+            return Polynomial(result_coeff)
+
+    def __str__(self):
         s = ''
-        for x in np.linspace(L, R, n):
-            y = self(x)
-            s += '%12g %12g\n' % (x, y)
-        return s
+        for i in range(0, len(self.coeff)):
+            if self.coeff[i] != 0:
+                s += ' + %g*x^%d' % (self.coeff[i], i)
+            # Fix layout
+            s = s.replace('+ -', '- ')
+            s = s.replace('x^0', '1')
+            s = s.replace(' 1*', ' ')
+            s = s.replace('x^1 ', 'x ')
+            # s = s.replace('x^1', 'x') # will replace x^100 by x^00
+            if s[0:3] == ' + ':  # remove initial +
+                s = s[3:]
+            if s[0:3] == ' - ':  # fix spaces for initial -
+                s = '-' + s[3:]
+            return s
 
+"""
+The exercise text asks for a different type of constructor
+for classes Parabola an Line, which takes the coeffecients as
+separate arguments. The classes must therefore implement their
+own constructor, while the rest can be inherited from the
+Polynomial base class.
+"""
+
+class Parabola(Polynomial):
+    def __init__(self, c0, c1, c2):
+        self.coeff = [c0, c1, c2]
+
+class Line(Polynomial):
+    def __init__(self, c0, c1):
+        self.coeff = [c0, c1]
+
+"""
+Example usage: Parabola and Line support pretty print
+and addition because of inheritance from the
+Polynomial base class.
+"""
+
+p0 = Parabola(1, 2, 3)
+print(p0)
+
+l0 = Line(5, 4)
+print(l0)
+
+p1 = l0 + p0
+print(p1)
 
 
